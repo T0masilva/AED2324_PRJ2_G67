c3ad27797e7fd35a256c35001559e2fa0da0b863
Tomás Cruz e Silva <up202005846@up.pt>
2023-12-05 09:56:14 +0000

    first commit

---
+++ src/Application.cpp
@@ -0,0 +1,5 @@
+//
+// Created by up202005846 on 05/12/2023.
+//
+
+#include "Application.h"
---
+++ src/Application.h
@@ -0,0 +1,14 @@
+//
+// Created by up202005846 on 05/12/2023.
+//
+
+#ifndef AED2324_PRJ2_G67_APPLICATION_H
+#define AED2324_PRJ2_G67_APPLICATION_H
+
+
+class Application {
+
+};
+
+
+#endif //AED2324_PRJ2_G67_APPLICATION_H
